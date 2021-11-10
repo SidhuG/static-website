d142abc7bf0e5949060b65e0e4f16bd55be47ba0
Gautam MacMini <gtmsidhu@icloud.com>
2021-11-10 13:37:00 +0000

    static website on s3

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.64.2"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+  }
+
+  required_version = "~> 0.14"
+}
